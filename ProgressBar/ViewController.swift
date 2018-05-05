b56c1c50d8266c6992131b10d003ae6136dff83b
Rajesh <raj.8130052435@gmail.com>
2018-05-05 23:14:51 +0530

    Progress Bar

--- ProgressBar/ViewController.swift
+++ ProgressBar/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    let MAXTIME : Float = 10.0
+    var currentTime : Float = 0.0
+    
+    // MARK: IBOutlet
+    @IBOutlet weak var labelTime: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    // MARK: ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: IBAction
+    @IBAction func performAction(_ sender: UIButton) {
+        
+        progressView.setProgress(currentTime, animated: true)
+        perform(#selector(updateProgress), with: nil, afterDelay: 1.0)
+        
     }
-
-
+    
+    
+    @objc func updateProgress(){
+        
+        currentTime = currentTime + 1.0
+        progressView.progress = currentTime/MAXTIME
+        labelTime.text = "\(currentTime)"
+        
+        if currentTime < MAXTIME{
+            perform(#selector(updateProgress), with: nil, afterDelay: 1.0)
+        }else{
+            print("Stop")
+            currentTime = 0.0
+        }
+    }
+    
 }
 
